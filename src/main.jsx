9e588b831ead053fe93dd0cca6506545d974ff53
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-11 15:14:05 +0200

    react static page 'fun facts about react'

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import reactLogo from './assets/react.svg'
 import './index.css'
 
 const page = (
   <div>
-      <h1>My awesome website in React</h1>
-      <h3>Reasons I love React</h3>
-      <ol>
-          <li>It's composable</li>
-          <li>It's declarative</li>
-          <li>It's a hireable skill</li>
-          <li>It's actively maintained by skilled people</li>
-      </ol>
+    <img src={reactLogo} alt="" />
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on Github</li>
+      <li>Is maintained by Meta</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
   </div>
 )
 
