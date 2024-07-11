af6d177eb7f06a39865a6b35d57ee1710e64029c
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-11 15:04:53 +0200

    another simple react page

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const navBar = (
-  <nav>
-    <h1>my Website</h1>
-    <ul><li>item 1</li><li>item 2</li></ul>
-  </nav>
+const page = (
+  <div>
+      <h1>My awesome website in React</h1>
+      <h3>Reasons I love React</h3>
+      <ol>
+          <li>It's composable</li>
+          <li>It's declarative</li>
+          <li>It's a hireable skill</li>
+          <li>It's actively maintained by skilled people</li>
+      </ol>
+  </div>
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  navBar
+  page
 )
