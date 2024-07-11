7d72bd1fec05f5bab3fc0c89c114553a69e1d499
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-11 15:24:08 +0200

    another simple site, now as proper react component

--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,21 @@ import App from './App.jsx'
 import reactLogo from './assets/react.svg'
 import './index.css'
 
-const page = (
-  <div>
-    <img src={reactLogo} alt="" />
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on Github</li>
-      <li>Is maintained by Meta</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-)
+function Page(){
+  return (
+    <div>
+      <img src={reactLogo}></img>
+      <h1>Why I am excited to learn react</h1>
+      <ul>
+        <li>Modern UI Development: Build dynamic and responsive user interfaces.</li>
+        <li>Component-Based Architecture: Reusable components streamline development.</li>
+        <li>Large Community: Abundant resources, support, and libraries.</li>
+        <li>High Demand: Valuable skill in the job market.</li>
+      </ul>
+    </div>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  page
+  <Page />
 )
