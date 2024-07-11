9ae417d0c5db68de5e3e38c8904857c6c7334065
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-11 14:46:31 +0200

    add navbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const navBar = (
+  <nav>
+    <h1>my Website</h1>
+    <ul><li>item 1</li><li>item 2</li></ul>
+  </nav>
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <h1>This is JSX</h1>
+  navBar
 )
