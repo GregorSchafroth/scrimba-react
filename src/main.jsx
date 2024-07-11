9082170130a3410270f3399a7f28fa60252a740f
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-11 15:37:14 +0200

    using multiple components

--- src/main.jsx
+++ src/main.jsx
@@ -4,21 +4,34 @@ import App from './App.jsx'
 import reactLogo from './assets/react.svg'
 import './index.css'
 
+function Header(){
+  return(
+    <header>
+      <img src={reactLogo}></img>
+      <h1>Why I am excited to learn react</h1>
+    </header>
+  )
+}
+
 function Page(){
   return (
     <div>
-      <img src={reactLogo}></img>
-      <h1>Why I am excited to learn react</h1>
       <ul>
         <li>Modern UI Development: Build dynamic and responsive user interfaces.</li>
         <li>Component-Based Architecture: Reusable components streamline development.</li>
         <li>Large Community: Abundant resources, support, and libraries.</li>
         <li>High Demand: Valuable skill in the job market.</li>
       </ul>
+      <footer>
+        <small>© 2024 Gregor Schafroth. All rights reserved.</small>
+      </footer>
     </div>
   )
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Page />
+  <div>
+    <Header />
+    <Page />
+  </div>
 )
